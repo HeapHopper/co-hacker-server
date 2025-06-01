3ad4f925fadd9830cac11d3b043653166ab9f57f
heaphopper <tzvienir@gmail.com>
2025-06-01 18:08:50 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from models import SnippetInput, CodeSnippet, mock_vulnerable_snippet
+from graph import build_graph, GraphState
+
+app = FastAPI()
+
+# Optional CORS for local dev or VSCode extension
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Build LangGraph
+graph = build_graph()
+
+@app.post("/analyze", response_model=CodeSnippet)
+async def analyze_snippet(input: SnippetInput):
+    try:
+        result_state = graph.invoke(GraphState(input=input))
+        print()
+        # return result_state.output
+        return result_state['output']
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+class SnippetInput(BaseModel):
+    snippet: str
+
+class CodeSnippet(BaseModel):
+    is_vulnerable: bool
+    vulnerability_type: str
+    vulnerability: str
+    suggest_fix: str
+
+
+mock_safe_snippet = CodeSnippet(
+            is_vulnerable="False",
+            vulnerability_type="",
+            vulnerability="",
+            suggest_fix=""
+        )
+
+mock_vulnerable_snippet = CodeSnippet(
+            is_vulnerable="True",
+            vulnerability_type="test vulnerability",
+            vulnerability="this is a test vulnerability",
+            suggest_fix="/* This is a test fix */\n"
+        )
