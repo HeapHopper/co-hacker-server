6303d9daab49b1e5e1cd817c542bc2669d9375a7
heaphopper <tzvienir@gmail.com>
2025-06-18 21:53:55 +0300

    define used Models in feature

---
+++ src/inline_assistant/inline_assistnat_models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class Vulnerability(BaseModel):
+    description: str
+    vulnerable_code: str
+
+### AI Models for Code Snippet Analysis
+
+class InlineAssistantRequest(BaseModel):
+    current_line: str
+    current_scope: str
+    current_file: str
+
+class InlineAssistantResponse(BaseModel):
+    is_vulnerable: bool
+    vulnerability: Vulnerability
+    suggest_fix: str
+
+#TODO add support for multiple vulnerabilities
